186e739962008a7dc399c11d4474a82f99d7f5d6
Teodor Birghisan <birghisanteodor@yahoo.com>
2022-05-28 00:57:56 +0300

    Added classes to app 📔

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CandidateModule } from './modules/candidate/candidate.module';
+import { ConsulationModule } from './modules/consultation/consultation.module';
+import { LocationModule } from './modules/location/location.module';
+import { TutorModule } from './modules/tutor/tutor.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TutorModule,
+    CandidateModule,
+    LocationModule,
+    TutorModule,
+    ConsulationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
