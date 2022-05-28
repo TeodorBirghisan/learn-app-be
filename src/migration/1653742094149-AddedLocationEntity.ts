b19f3da774a4a90eaf13fd2d573498d66ddfae51
Teodor Birghisan <birghisanteodor@yahoo.com>
2022-05-28 16:15:53 +0300

    format

--- src/migration/1653742094149-AddedLocationEntity.ts
+++ src/migration/1653742094149-AddedLocationEntity.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddedLocationEntity1653742094149 implements MigrationInterface {
-    name = 'AddedLocationEntity1653742094149'
+  name = 'AddedLocationEntity1653742094149';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "location" ("id" SERIAL NOT NULL, "country" character varying NOT NULL, "county" character varying NOT NULL, "city" character varying NOT NULL, "street" character varying NOT NULL, "number" character varying NOT NULL, "apartment" character varying NOT NULL, CONSTRAINT "PK_876d7bdba03c72251ec4c2dc827" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "location" ("id" SERIAL NOT NULL, "country" character varying NOT NULL, "county" character varying NOT NULL, "city" character varying NOT NULL, "street" character varying NOT NULL, "number" character varying NOT NULL, "apartment" character varying NOT NULL, CONSTRAINT "PK_876d7bdba03c72251ec4c2dc827" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "location"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "location"`);
+  }
 }
