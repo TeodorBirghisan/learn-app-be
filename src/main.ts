bd4b81c62f13ac990baf6e72446a84bbb67ac8c8
Teodor Birghisan <birghisanteodor@yahoo.com>
2022-05-28 12:12:22 +0300

    Changed default port 🎗

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 6080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -15,6 +15,7 @@ async function bootstrap() {
     .setDescription('Learn App API description')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
