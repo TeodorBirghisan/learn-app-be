c7c78651a9be09d9d99b8df4c2090f2a0bf30c46
Teodor Birghisan <birghisanteodor@yahoo.com>
2022-05-28 12:18:37 +0300

    Formated code 🎈

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import { AppModule } from './app.module';
 const PORT = process.env.PORT || 6080;
 
 async function bootstrap() {
-
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
--- src/migration/1653726087696-CreateUserEntity.ts
+++ src/migration/1653726087696-CreateUserEntity.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUserEntity1653726087696 implements MigrationInterface {
-    name = 'CreateUserEntity1653726087696'
+  name = 'CreateUserEntity1653726087696';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "role" character varying, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "role" character varying, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
 }
